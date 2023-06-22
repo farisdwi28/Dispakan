5a83e9ad7b71fcc0d5a6f97901c199a65a597fc1
aditya <adityafernanda2013@gmail.com>
2023-06-22 17:28:12 +0700

    feature: fullsection

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,14 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ["Poppins", "ui-sans-serif", "system-ui"]
+      },
+      colors: {
+        primary1: "#D36B16",
+        primary2: "#E4283D",
+        primary3: "#515D6A",
+        bgColor1: "#D36B16",
+        bgColor2: "#E4283D",
+        bgColor3: "#515D6A"
       }
     }
   },
