89a5b014ce2f986a3495ec733b6803d5a59b63e3
aditya <adityafernanda2013@gmail.com>
2023-06-25 15:27:35 +0700

    feature: regiter bumdes

--- src/components/FormRegister.js
+++ src/components/FormRegister.js
@@ -1,26 +1,27 @@
 import * as React from "react";
 import { images } from "../constans";
 
-export default function FormLogin() {
+export default function FormRegister() {
   return (
     <div className="bg-white object-center rounded-3xl border-2 border-primary3-100 px-8 py-8">
       <h2 className="text-3xl font-semibold text-center items-center text-primary1">
         Dispakan.
       </h2>
       <h2 className="text-2xl font-semibold">Selamat datang kembali</h2>
-      <p className="font-medium text-lg text-primary3">
-        Masuk sebagai BUMDES atau UMKM
-      </p>
+      <p>Daftar akun hanya diperuntukan BUMDES</p>
       <div className="mt-8">
         <div>
-          <label className="text-lg font-medium px-2">
-            Email atau username
-          </label>
+          <label className="text-lg font-medium px-2">Email</label>
           <input className="w-full border-2 border-gray-100 rounded-xl p-4 mt-2 bg-transparent" />
         </div>
 
         <div>
-          <label className="text-lg font-medium px-2">Password</label>
+          <label className="text-lg font-medium px-2">Kata Sandi</label>
+          <input className="w-full border-2 border-gray-100 rounded-xl p-4 mt-2 bg-transparent" />
+        </div>
+
+        <div>
+          <label className="text-lg font-medium px-2">Ulang Kata Sandi</label>
           <input className="w-full border-2 border-gray-100 rounded-xl p-4 mt-2 bg-transparent" />
         </div>
 
@@ -31,18 +32,15 @@ export default function FormLogin() {
               Ingatkan saya
             </label>
           </div>
-          <button className="font-medium text-base text-primary1">
-            Lupa kata sandi?
-          </button>
         </div>
 
         <div className="mt-8 flex flex-col gap-y-4">
           <button className="active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out py-3 rounded-xl bg-primary1 text-white text-lg font-bold">
-            Masuk
+            Daftar
           </button>
           <button className="flex items-center justify-center gap-2 active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out">
             <img src={images.google} className="w-8 h-8" />
-            <span className="align-middle">Masuk dengan Google</span>
+            <span className="align-middle">Daftar dengan Google</span>
           </button>
         </div>
       </div>
