9f729913a5b513233da3bb724ce5c1cc674fea1a
aditya <adityafernanda2013@gmail.com>
2023-06-25 23:54:54 +0700

    fix: responsive login regiter page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = withMT({
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
     "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
-    "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
